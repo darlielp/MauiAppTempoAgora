d0a6e2a959825e3d39e7109dd219f0058fe72f05
darlielp <darlielpereira@protonmail.com>
2026-04-05 13:55:40 -0300

    conecta à API OpenWeather e exibe informações

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,24 +1,49 @@
-﻿namespace MauiAppTempoAgora
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+using System.Linq.Expressions;
+
+namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
+       
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            try{
+                if(!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
+                    if (t != null) {
+                        string dados_previsao = "";
+                        dados_previsao = $"Latitude: {t.lat} \n" +
+                                         $"Longitude: {t.lon} \n" +
+                                         $"Nascer do Sol: {t.sunrise} \n" +
+                                         $"Por do Sol: {t.sunset} \n" +
+                                         $"Temp Máx: {t.temp_max}°C \n" +
+                                         $"Temp Mín: {t.temp_min}°C \n" +
+                                         $"Descrição do clima: {t.description} \n" +
+                                         $"Velocidade do vento: {t.speed} \n" +
+                                         $"Visibilidade: {t.visibility / 1000} Km \n";
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                        lbl_res.Text = dados_previsao;
+                    } else
+                    {
+                        lbl_res.Text = "Sem dados de previsão.";
+                    }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                } else
+                {
+                    lbl_res.Text = "Preencha a cidade.";
+                }
+            } catch(Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
         }
     }
 }
